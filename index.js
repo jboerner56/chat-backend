e1e2e1951878bef94a34f8c26e07728fbbbba9a7
Jason Boerner <jboerner56@gamil.com>
2019-05-02 14:14:09 -0400

    post and get

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
+app.use(express.urlencoded({extended: true}));
+// this is the database for now
+const db = [
+    'this is the app/ current database'
+];
 
+// when Get request comes in, send back all messages
+app.get('/', (req, res) => {
+    res.json(db);
+});
+
+//when post request comes in, add message to array of messages
+app.post('/', (req, res) => {
+    console.log(req.body.message);
+    db.push(req.body.message);
+    res.json({
+        'message': req.body.message
+    })
+});
 app.listen(3000, () => {
     console.log('App is running');
 });
