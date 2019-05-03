fead92a029240929bc54f4732f5de847c929e15c
Jason Boerner <jboerner56@gamil.com>
2019-05-03 14:09:24 -0400

    wedsocket backend

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
 const express = require('express');
+const http = require('http');
 const app = express();
+//  create plain server that uses express app
+const server = http.createServer(app);
+
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({
+    server,  // piggybacking on plain http server
+    path: '/chat' // listen on only one route, allows express to continue to listen on custom routes
+});
+
 app.use(express.urlencoded({extended: true}));
 // this is the database for now
 const db = [
     'this is the app/ current database'
 ];
+
+wss.on('connection', (socket) => {
+    console.log('new connection');
+    socket.send(JSON.stringify(db));
+    socket.on('message', (data) => {
+        console.log(data);
+        db.push(data);
+        console.log(db);
+        wss.clients.forEach((client) => {
+            if(client.readyState === WebSocket.OPEN){
+                client.send(JSON.stringify(data));
+            }
+        });
+        // socket.send(data);
+
+    });
+});
 
 // when Get request comes in, send back all messages
 app.get('/api', (req, res) => {
@@ -20,6 +47,6 @@ app.post('/api', (req, res) => {
         
     })
 });
-app.listen(3000, () => {
+server.listen(3000, () => {
     console.log('App is running');
 });
